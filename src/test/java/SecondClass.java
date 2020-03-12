8dbe8b7b39ad9aec0eed3eb1d4b35476ede7de3a
Munargul Shaiyrbekova <munara682@gmail.com>
2020-03-11 21:22:42 -0400

    third commit

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -3,5 +3,6 @@ public class SecondClass {
         System.out.println("Please Batch 15 be patient with him");
         System.out.println("Hello");
         System.out.println("Kyrgyzstan");
+        System.out.println("Play Dead by Harlan Coben");
     }
 }
