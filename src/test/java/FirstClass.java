dd5c920930f1484131f337945c3442f33a320cb2
munara90 <58119556+munara90@users.noreply.github.com>
2020-03-18 20:05:03 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,7 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Hey, Hey , Hey");
-        System.out.println("Hey!");
+        System.out.println("Chnge one");
         
         
 
